79183e89ceeec83e56c8310259f8511428ff57ce
Ishan Sharma <sharmaishan747@gmail.com>
2020-02-23 00:56:14 +0530

    Push Hash fixed and working

--- Stage1/parser.c
+++ Stage1/parser.c
@@ -24,6 +24,348 @@ int stack[100];
 int stack_max=99; 	//max value for indexing stack
 int top=-1; 		// top pointer
 
+Term t[111];
+
+
+/* HARD CODE the terms here*/
+/*
+	strcpy(t[0].termname,"$");
+	t[0].nont=false;
+
+	// Non-Terminals 
+
+	strcpy(t[1].termname,"program");
+	t[1].nont=true;
+
+	strcpy(t[2].termname,"moduleDeclarations");
+	t[2].nont=true;
+
+	strcpy(t[3].termname,"moduleDeclaration");
+	t[3].nont=true;
+
+	strcpy(t[4].termname,"otherModules");
+	t[4].nont=true;
+
+	strcpy(t[5].termname,"driverModule");
+	t[5].nont=true;
+
+	strcpy(t[6].termname,"module");
+	t[6].nont=true;
+
+	strcpy(t[7].termname,"ret");
+	t[7].nont=true;
+
+	strcpy(t[8].termname,"input_plist");
+	t[8].nont=true;
+
+	strcpy(t[9].termname,"input_plist_again");
+	t[9].nont=true;
+
+	strcpy(t[10].termname,"output_plist");
+	t[10].nont=true;
+
+	strcpy(t[11].termname,"output_plist_again");
+	t[11].nont=true;
+
+	strcpy(t[12].termname,"dataType");
+	t[12].nont=true;
+
+	strcpy(t[13].termname,"type");
+	t[13].nont=true;
+
+	strcpy(t[14].termname,"moduleDef");
+	t[14].nont=true;
+
+	strcpy(t[15].termname,"statements");
+	t[15].nont=true;
+
+	strcpy(t[16].termname,"statement");
+	t[16].nont=true;
+
+	strcpy(t[17].termname,"ioStmt");
+	t[17].nont=true;
+
+	strcpy(t[18].termname,"whichId");
+	t[18].nont=true;
+
+	strcpy(t[19].termname,"simpleStmt");
+	t[19].nont=true;
+
+	strcpy(t[20].termname,"assignmentStmt");
+	t[20].nont=true;
+
+	strcpy(t[21].termname,"whichStmt");
+	t[21].nont=true;
+
+	strcpy(t[22].termname,"lvalueIDStmt");
+	t[22].nont=true;
+
+	strcpy(t[23].termname,"lvalueARRStmt");
+	t[23].nont=true;
+
+	strcpy(t[24].termname,"index");
+	t[24].nont=true;
+
+	strcpy(t[25].termname,"moduleReuseStmt");
+	t[25].nont=true;
+
+	strcpy(t[26].termname,"optional");
+	t[26].nont=true;
+
+	strcpy(t[27].termname,"idList");
+	t[27].nont=true;
+
+	strcpy(t[28].termname,"idList_again");
+	t[28].nont=true;
+
+	strcpy(t[29].termname,"expression");
+	t[29].nont=true;
+
+	strcpy(t[30].termname,"unary");
+	t[30].nont=true;
+
+	strcpy(t[31].termname,"arithmeticOrBooleanExpr");
+	t[31].nont=true;
+
+	strcpy(t[32].termname,"arithmeticOrBooleanExpr_again");
+	t[32].nont=true;
+
+	strcpy(t[33].termname,"recTerm");
+	t[33].nont=true;
+
+	strcpy(t[34].termname,"recTerm_again");
+	t[34].nont=true;
+
+	strcpy(t[35].termname,"arithmeticExpr");
+	t[35].nont=true;
+
+	strcpy(t[36].termname,"arithmeticExpr_again");
+	t[36].nont=true;
+
+	strcpy(t[37].termname,"term");
+	t[37].nont=true;
+
+	strcpy(t[38].termname,"term_again");
+	t[38].nont=true;
+
+	strcpy(t[39].termname,"factor");
+	t[39].nont=true;
+
+	strcpy(t[40].termname,"var");
+	t[40].nont=true;
+
+	strcpy(t[41].termname,"booleanConstants");
+	t[41].nont=true;
+
+	strcpy(t[42].termname,"prec2_op");
+	t[42].nont=true;
+
+	strcpy(t[43].termname,"prec1_op");
+	t[43].nont=true;
+
+	strcpy(t[44].termname,"logicalOp");
+	t[44].nont=true;
+
+	strcpy(t[45].termname,"relationalOp");
+	t[45].nont=true;
+
+	strcpy(t[46].termname,"declareStmt");
+	t[46].nont=true;
+
+	strcpy(t[47].termname,"conditionalStmt");
+	t[47].nont=true;
+
+	strcpy(t[48].termname,"caseStmts");
+	t[48].nont=true;
+
+	strcpy(t[49].termname,"caseStmts_again");
+	t[49].nont=true;
+
+	strcpy(t[50].termname,"value");
+	t[50].nont=true;
+
+	strcpy(t[51].termname,"default");
+	t[51].nont=true;
+
+	strcpy(t[52].termname,"iterativeStmt");
+	t[52].nont=true;
+
+	strcpy(t[53].termname,"range");
+	t[53].nont=true;
+
+	// Terminals 
+
+	strcpy(t[54].termname,"AND");
+	t[54].nont=false;;
+
+	strcpy(t[55].termname,"ARRAY");
+	t[55].nont=false;;
+
+	strcpy(t[56].termname,"ASSIGNOP");
+	t[56].nont=false;;
+
+	strcpy(t[57].termname,"BC");
+	t[57].nont=false;;
+
+	strcpy(t[58].termname,"BO");
+	t[58].nont=false;;
+
+	strcpy(t[59].termname,"BOOLEAN");
+	t[59].nont=false;
+
+	strcpy(t[60].termname,"BREAK");
+	t[60].nont=false;
+
+	strcpy(t[61].termname,"CASE");
+	t[61].nont=false;
+
+	strcpy(t[62].termname,"COLON");
+	t[62].nont=false;
+
+	strcpy(t[63].termname,"COMMA");
+	t[63].nont=false;
+
+	strcpy(t[64].termname,"DECLARE");
+	t[64].nont=false;
+
+	strcpy(t[65].termname,"DEF");
+	t[65].nont=false;
+
+	strcpy(t[66].termname,"DEFAULT");
+	t[66].nont=false;
+
+	strcpy(t[67].termname,"DIV");
+	t[67].nont=false;
+
+	strcpy(t[68].termname,"DRIVER");
+	t[68].nont=false;
+
+	strcpy(t[69].termname,"DRIVERDEF");
+	t[69].nont=false;
+
+	strcpy(t[70].termname,"DRIVERENDDEF");
+	t[70].nont=false;
+
+	strcpy(t[71].termname,"END");
+	t[71].nont=false;
+
+	strcpy(t[72].termname,"ENDDEF");
+	t[72].nont=false;
+
+	strcpy(t[73].termname,"EQ");
+	t[73].nont=false;
+
+	strcpy(t[74].termname,"FALSE");
+	t[74].nont=false;
+
+	strcpy(t[75].termname,"FOR");
+	t[75].nont=false;
+
+	strcpy(t[76].termname,"GE");
+	t[76].nont=false;
+
+	strcpy(t[77].termname,"GET_VALUE");
+	t[77].nont=false;
+
+	strcpy(t[78].termname,"GT");
+	t[78].nont=false;
+
+	strcpy(t[79].termname,"ID");
+	t[79].nont=false;
+
+	strcpy(t[80].termname,"IN");
+	t[80].nont=false;
+
+	strcpy(t[81].termname,"INPUT");
+	t[81].nont=false;
+
+	strcpy(t[82].termname,"INTEGER");
+	t[82].nont=false;
+
+	strcpy(t[83].termname,"LE");
+	t[83].nont=false;
+
+	strcpy(t[84].termname,"LT");
+	t[84].nont=false;
+
+	strcpy(t[85].termname,"MINUS");
+	t[85].nont=false;
+
+	strcpy(t[86].termname,"MODULE");
+	t[86].nont=false;
+
+	strcpy(t[87].termname,"MUL");
+	t[87].nont=false;
+
+	strcpy(t[88].termname,"NE");
+	t[88].nont=false;
+
+	strcpy(t[89].termname,"NUM");
+	t[89].nont=false;
+
+	strcpy(t[90].termname,"OF");
+	t[90].nont=false;
+
+	strcpy(t[91].termname,"OR");
+	t[91].nont=false;
+
+	strcpy(t[92].termname,"PARAMETERS");
+	t[92].nont=false;
+
+	strcpy(t[93].termname,"PLUS");
+	t[93].nont=false;
+
+	strcpy(t[94].termname,"PRINT");
+	t[94].nont=false;
+
+	strcpy(t[95].termname,"PROGRAM");
+	t[95].nont=false;
+
+	strcpy(t[96].termname,"RANGEOP");
+	t[96].nont=false;
+
+	strcpy(t[97].termname,"REAL");
+	t[97].nont=false;
+
+	strcpy(t[98].termname,"RETURNS");
+	t[98].nont=false;
+
+	strcpy(t[99].termname,"RNUM");
+	t[99].nont=false;
+
+	strcpy(t[100].termname,"SEMICOL");
+	t[100].nont=false;
+
+	strcpy(t[101].termname,"SQBC");
+	t[101].nont=false;
+
+	strcpy(t[102].termname,"SQBO");
+	t[102].nont=false;
+
+	strcpy(t[103].termname,"START");
+	t[103].nont=false;
+
+	strcpy(t[104].termname,"SWITCH");
+	t[104].nont=false;
+
+	strcpy(t[105].termname,"TAKES");
+	t[105].nont=false;
+
+	strcpy(t[106].termname,"TRUE");
+	t[106].nont=false;
+
+	strcpy(t[107].termname,"USE");
+	t[107].nont=false;
+
+	strcpy(t[108].termname,"WHILE");
+	t[108].nont=false;
+
+	strcpy(t[109].termname,"WITH");
+	t[109].nont=false;
+
+	strcpy(t[110].termname,"EPSILON");
+	t[110].nont=false;
+*/
 /* **************************************************************************** */
 
 
@@ -83,8 +425,9 @@ Grammar getGrammar(FILE *f)
 
 			do
 			{
-				if(word[g][x]=='\n')
+				if(!((word[g][x]>='a') && (word[g][x]<='z') || (word[g][x]>='A') && (word[g][x]<='Z')))
 				{
+					//fprintf(f2,"\n \n %d \n \n",word[g][x]);
 					x++;
 				}
 				else
@@ -95,14 +438,14 @@ Grammar getGrammar(FILE *f)
 
 			}while(word[g][x]!='\0');
 
-			fprintf(f2,"%s",toinsert);
+			//fprintf(f2,"%s",toinsert);
 
 			int hashVal=insert(toinsert,TermTable);
-			//fprintf(f2,"%d ",hashVal);
+			fprintf(f2,"%d ",hashVal);
 			//G.gnum[rule_no][g]=hashVal;
 		}
 
-		//fprintf(f2,"\n");
+		fprintf(f2,"\n");
 
 		//G.gnum[rule_no][g]=-1;
 		//rule_no++;
